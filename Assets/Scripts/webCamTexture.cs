14e112b0e029c3bb5ea524eaa345263021b95f67
sandesh hegde <sandeshhegde1@gmail.com>
2017-09-29 13:19:57 -0700

    V1.0

---
+++ Assets/Scripts/webCamTexture.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class webCamTexture : MonoBehaviour {
+
+	public GameObject webCamPlane;
+	public Button fireButton;
+	// Use this for initialization
+	void Start () {
+
+		if (Application.isMobilePlatform) {
+			GameObject cameraParent = new GameObject ("camParent");
+			cameraParent.transform.position = this.transform.position;
+			this.transform.parent = cameraParent.transform;
+			cameraParent.transform.Rotate (Vector3.right, 90);
+		}
+
+		Input.gyro.enabled = true;
+
+		fireButton.onClick.AddListener (OnFireButtonClick);
+
+		WebCamTexture webCamTexture = new WebCamTexture ();
+		webCamPlane.GetComponent<MeshRenderer>().material.mainTexture = webCamTexture;
+		webCamTexture.Play ();
+	}
+
+	void OnFireButtonClick(){
+
+		GameObject bullet = Instantiate(Resources.Load("Bullet", typeof(GameObject))) as GameObject;
+		Rigidbody rb = bullet.GetComponent<Rigidbody>();
+		bullet.transform.rotation = Camera.main.transform.rotation;
+		bullet.transform.position = Camera.main.transform.position;
+		rb.AddForce(Camera.main.transform.forward * 500f);
+		GetComponent<AudioSource> ().Play ();
+		Destroy (bullet, 3);
+
+
+
+	}
+	// Update is called once per frame
+	void Update () {
+		Quaternion cameraRotation = new Quaternion (Input.gyro.attitude.x, Input.gyro.attitude.y, -Input.gyro.attitude.z, -Input.gyro.attitude.w);
+		this.transform.localRotation = cameraRotation;
+	}
+}
---
+++ Assets/collisionScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collisionScript : MonoBehaviour {
+
+	public AudioClip explosionClip;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//for this to work both need colliders, one must have rigid body (spaceship) the other must have is trigger checked.
+	void OnTriggerEnter (Collider col)
+	{
+		if(col.gameObject.CompareTag("Player")){
+			
+			GameObject explosion = Instantiate(Resources.Load("FlareMobile", typeof(GameObject))) as GameObject;
+			explosion.transform.position = transform.position;
+			Destroy(col.gameObject);
+			Destroy (explosion, 2);
+			AudioSource.PlayClipAtPoint (explosionClip,col.transform.position);
+
+
+			//if all the enemy ships are destroyed, then instantiate them again.
+			if (GameObject.FindGameObjectsWithTag("Player").Length == 0){
+
+				GameObject enemy1 = Instantiate(Resources.Load("enemy1", typeof(GameObject))) as GameObject;
+				GameObject enemy2 = Instantiate(Resources.Load("enemy2", typeof(GameObject))) as GameObject;
+				GameObject enemy3 = Instantiate(Resources.Load("enemy3", typeof(GameObject))) as GameObject;
+				GameObject enemy4 = Instantiate(Resources.Load("enemy4", typeof(GameObject))) as GameObject;
+
+			}
+
+			//destroy the bullet
+			Destroy (gameObject);
+
+		}
+
+
+	}
+}
